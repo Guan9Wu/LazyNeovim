bd5187cd5de5feeb6820d097c245039622dd8e7f
ArchWSL_Lenovo <gene9@foxmail.com>
2022-10-20 23:34:58 +0800

    Change deprecated func and fix null-ls conflict. 	modified:   lua/modules/lang/lspconfigure.lua

--- lua/modules/lang/lspconfigure.lua
+++ lua/modules/lang/lspconfigure.lua
@@ -77,14 +77,14 @@ require("mason-lspconfig").setup({
     -- A list of servers to automatically install if they're not already installed. Example: { "rust_analyzer@nightly", "sumneko_lua" }
     -- This setting has no relation with the `automatic_installation` setting.
     ensure_installed = {
-        "lua-language-server",
+        "sumneko_lua",
         "clangd",
         "pyright",
-        "stylua",
-        "black",
-        "clang-format",
-        "codelldb",
-        "debugpy",
+        --"stylua",
+        --"black",
+        --"clang-format",
+        --"codelldb",
+        --"debugpy",
     },
     -- Whether servers that are set up (via lspconfig) should be automatically installed if they're not already installed.
     -- This setting has no relation with the `ensure_installed` setting.
@@ -126,7 +126,34 @@ local isWindows = vim.loop.os_uname().version:match("Windows")
 --     vim.keymap.set('n', '<space>lt', function() vim.lsp.buf.format { async = true } end, bufopts)
 -- end
 
-local capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+local lsp_formatting = function(bufnr)
+    vim.lsp.buf.format({
+        filter = function(client)
+            -- apply whatever logic you want (in this example, we'll only use null-ls)
+            return client.name == "null-ls"
+        end,
+        bufnr = bufnr,
+    })
+end
+
+-- if you want to set up formatting on save, you can use this as a callback
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+
+-- add to your shared on_attach callback
+local on_attach = function(client, bufnr)
+    if client.supports_method("textDocument/formatting") then
+        vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+        vim.api.nvim_create_autocmd("BufWritePre", {
+            group = augroup,
+            buffer = bufnr,
+            callback = function()
+                lsp_formatting(bufnr)
+            end,
+        })
+    end
+end
 
 -- Use a loop to conveniently call 'setup' on multiple servers and
 -- map buffer local keybindings when the language server attaches
@@ -192,6 +219,8 @@ else
         cmd = {
             "clangd",
             "--pretty",
+            "--offset-encoding=utf-16",
+            "--background-index",
         },
     })
 end
