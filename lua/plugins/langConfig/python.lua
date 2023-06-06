e059c419508f312adc222ac0fa0cf9a6d80bb309
EndeavourOS <gene9@foxmail.com>
2023-06-06 13:10:27 +0800

    disable pylint

--- lua/plugins/langConfig/python.lua
+++ lua/plugins/langConfig/python.lua
@@ -40,7 +40,7 @@ return {
     "jose-elias-alvarez/null-ls.nvim",
     opts = function(_, opts)
       local nls = require("null-ls")
-      table.insert(opts.sources, nls.builtins.diagnostics.pylint)
+      -- table.insert(opts.sources, nls.builtins.diagnostics.pylint)
       table.insert(opts.sources, nls.builtins.formatting.black)
     end,
   },
