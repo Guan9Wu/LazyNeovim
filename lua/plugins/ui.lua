eb8d6aee09a82d1791499414fdca87b91b22a6ba
Arcolinux <gene9@foxmail.com>
2023-10-13 03:40:20 +0800

    misc

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -6,13 +6,6 @@ return {
     },
   },
   {
-    "nvim-lualine/lualine.nvim",
-    event = "VeryLazy",
-    opts = function(_, opts)
-      table.insert(opts.sections.lualine_x, "😄")
-    end,
-  },
-  {
     "uga-rosa/ccc.nvim",
     config = function()
       require("ccc").setup({
