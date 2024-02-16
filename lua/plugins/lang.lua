18a3224bdea35d8d38348274f6d12a0a55a20718
Guan9 <gene9@foxmail.com>
2024-02-16 21:24:36 +0800

    for C/C++

---
+++ lua/plugins/lang.lua
@@ -0,0 +1,22 @@
+return {
+  -- c/cpp
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        -- Ensure mason installs the server
+        clangd = {
+          cmd = {
+            "clangd",
+            "--background-index",
+            "--clang-tidy",
+            "--header-insertion=iwyu",
+            "--completion-style=detailed",
+            "--function-arg-placeholders",
+            "--fallback-style=google",
+          },
+        },
+      },
+    },
+  },
+}
