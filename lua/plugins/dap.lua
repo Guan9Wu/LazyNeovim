baf0373f5d711782b4d6956b160f09fbb9da815b
Guan9 <gene9@foxmail.com>
2024-07-17 01:33:31 +0800

    feat(undotree): add undotree plugin

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -8,27 +8,4 @@ return {
       all_frames = true, -- show virtual text for all stack frames not only current. Only works for debugpy on my machine.
     },
   },
-  {
-    "jay-babu/mason-nvim-dap.nvim",
-    dependencies = "mason.nvim",
-    cmd = { "DapInstall", "DapUninstall" },
-    opts = {
-      -- Makes a best effort to setup the various debuggers with
-      -- reasonable debug configurations
-      automatic_setup = false,
-
-      -- You can provide additional configuration to the handlers,
-      -- see mason-nvim-dap README for more information
-      handlers = {},
-
-      -- You'll need to check that you have the required things installed
-      -- online, please don't ask me how to install them :)
-      ensure_installed = {
-        -- Update this to ensure that you have the debuggers for the langs you want
-        "codelldb",
-        "python",
-        "bash",
-      },
-    },
-  },
 }
--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -1,61 +1,11 @@
 return {
-  -- change some telescope options and a keymap to browse plugin files
   {
-    "nvim-telescope/telescope.nvim",
-    dependencies = {
-      "nvim-telescope/telescope-fzf-native.nvim",
-      build = "make",
-      config = function()
-        require("telescope").load_extension("fzf")
-      end,
-    },
-    keys = {
-            -- add a keymap to browse plugin files
-            -- stylua: ignore
-            {
-              "<leader>fp",
-              function() require("telescope.builtin").find_files({ cwd = require("lazy.core.config").options.root }) end,
-              desc = "Find Plugin File",
-            },
-    },
-    -- change some options
-    opts = {
-      defaults = {
-        layout_strategy = "horizontal",
-        layout_config = { prompt_position = "top" },
-        sorting_strategy = "ascending",
-        winblend = 0,
-      },
-    },
-  },
-  -- trouble.nvim
-  {
-    "folke/trouble.nvim",
-    -- opts will be merged with the parent spec
-    opts = {
-      action_keys = { -- key mappings for actions in the trouble list
-        -- map to {} to remove a mapping, for example:
-        -- close = {},
-        close = "q", -- close the list
-        cancel = "<esc>", -- cancel the preview and get back to your last window / buffer / cursor
-        refresh = "r", -- manually refresh
-        jump = { "<cr>", "<tab>" }, -- jump to the diagnostic or open / close folds
-        open_split = { "s" }, -- open buffer in new split
-        open_vsplit = { "v" }, -- open buffer in new vsplit
-        open_tab = { "t" }, -- open buffer in new tab
-      },
-      use_diagnostic_signs = true, -- enabling this will use the signs defined in your lsp client
-    },
-  },
-  {
-    "mbbill/undotree",
-    config = function()
-      vim.g.undotree_SetFocusWhenToggle = 1
-      vim.g.undotree_WindowLayout = 2
-      vim.g.undotree_RelativeTimestamp = 0
-    end,
-    keys = {
-      { "<leader>uu", "<cmd>UndotreeToggle<cr>", desc = "Toggle UndoTree" },
+    -- UndoTree
+    "jiaoshijie/undotree",
+    dependencies = "nvim-lua/plenary.nvim",
+    config = true,
+    keys = { -- load the plugin only when using it's keybinding:
+      { "<leader>uu", "<cmd>lua require('undotree').toggle()<cr>", desc = "Toggle UndoTree" },
     },
   },
 }
